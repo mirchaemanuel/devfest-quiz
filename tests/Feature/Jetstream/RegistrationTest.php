a6ef32a611b5dfc823bb5193679eb45b129a498d
mirchaemanuel <mirchaemanuel@gmail.com>
2023-11-09 12:31:45 +0100

    cleaning tests

--- tests/Feature/Jetstream/RegistrationTest.php
+++ tests/Feature/Jetstream/RegistrationTest.php
@@ -12,14 +12,6 @@ test('registration screen can be rendered', function () {
     return ! Features::enabled(Features::registration());
 }, 'Registration support is not enabled.');
 
-test('registration screen cannot be rendered if support is disabled', function () {
-    $response = $this->get('/register');
-
-    $response->assertStatus(404);
-})->skip(function () {
-    return Features::enabled(Features::registration());
-}, 'Registration support is enabled.');
-
 test('new users can register', function () {
     $response = $this->post('/register', [
         'name' => 'Test User',

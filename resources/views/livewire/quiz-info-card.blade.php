e985054309e004ba70daa39a353dab3d68667813
mirchaemanuel <mirchaemanuel@gmail.com>
2023-11-14 17:08:09 +0100

    styles

--- resources/views/livewire/quiz-info-card.blade.php
+++ resources/views/livewire/quiz-info-card.blade.php
@@ -1,10 +1,12 @@
-<div class="group m-2 p-2 w-[350px] md:w-[400px] h-[250px] rounded border border-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 shadow-lg text-gray-900 dark:text-white">
-    <div class="h-full flex flex-col gap-2 justify-between items-stretch">
-        <h4 class="font-bold text-xl text-center">{{ $quiz->title }}</h4>
+<a class="m-2 rounded-lg border-2 border-gray-400 p-2 text-gray-900 shadow-lg group w-[350px] h-[250px]
+            hover:bg-gray-100 dark:text-white md:w-[400px] dark:hover:bg-gray-900 group"
+   href="{{ route('pages.members.quiz.show', $quiz) }}" wire:navigate>
+    <div class="flex h-full flex-col items-stretch justify-between gap-2">
+        <h4 class="text-center text-2xl font-bold">{{ $quiz->title }}</h4>
         <div class="flex items-center justify-center">
-            <img src="{{ Vite::asset('resources/assets/images/quizfest.png') }}" class="w-8 h-8" alt="image logo" title="LOGO" />
+            <img src="{{ Vite::asset('resources/assets/images/quizfest.png') }}" class="h-8 w-8" alt="image logo" title="LOGO" />
         </div>
-        <div class="flex flex-col justify-end items-stretch">
+        <div class="flex flex-col items-stretch justify-end">
             <div class="font-bold">
                 <span>{{ __('Questions') }}:</span> <span class="font-medium">{{ $quiz->questions->count() }}</span>
             </div>
@@ -12,11 +14,10 @@
                 <span>{{ __('Max score') }}:</span> <span class="font-medium">{{ $quiz->questions->sum('score') }}</span>
             </div>
             <div class="w-full text-right">
-                <a class="font-extrabold text-2xl hover:underline text-right cursor-pointer text-blue-600 dark:text-blue-200"
-                   href="{{ route('pages.members.quiz.show', $quiz) }}" wire:navigate>
-                    >{{ __('Play quiz') }}
-                </a>
+                <div class="cursor-pointer text-right text-2xl font-extrabold text-blue-600 group-hover:underline dark:text-blue-200">
+                    {{ __('Play quiz') }}
+                </div>
             </div>
         </div>
     </div>
-</div>
+</a>

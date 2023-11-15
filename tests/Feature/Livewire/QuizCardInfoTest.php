45d9ca8e8da04d0c956d407196f759ec04d4b3e2
mirchaemanuel <mirchaemanuel@gmail.com>
2023-11-15 12:45:03 +0100

    WIP: QuizCardInfo

--- tests/Feature/Livewire/QuizCardInfoTest.php
+++ tests/Feature/Livewire/QuizCardInfoTest.php
@@ -2,6 +2,21 @@
 
 use App\Models\Question;
 use App\Models\Quiz;
+use App\Models\User;
+
+it('has quiz property', function () {
+    // Arrange
+    $quiz = Quiz::factory()
+        ->create();
+    $user = User::factory()
+        ->create();
+
+    // Act & Assert
+    Livewire::test('quiz-info-card', ['quiz' => $quiz, 'user' => $user])
+        ->assertOk()
+        ->assertSet('quiz', $quiz);
+
+});
 
 it('has quiz name title', function () {
     // Arrange
@@ -37,11 +52,11 @@ it('has max score', function () {
 
     $question1 = Question::factory()->create([
         'quiz_id' => 1,
-        'score' => 5,
+        'score'   => 5,
     ]);
     $question2 = Question::factory()->create([
         'quiz_id' => 1,
-        'score' => 8,
+        'score'   => 8,
     ]);
 
     // Act & Assert
@@ -65,10 +80,95 @@ it('has play quiz link button', function () {
         ->assertOk()
         ->assertSeeHtmlInOrder([
             '<a',
-            'href="'.
+            'href="' .
             route('pages.members.quiz.show', $quiz),
             'wire:navigate',
             __('Play quiz'),
         ], false);
 
 });
+
+it('has user property', function () {
+    // Arrange
+    $quiz = Quiz::factory()
+        ->create();
+    $user = User::factory()
+        ->create();
+
+    // Act & Assert
+    Livewire::test('quiz-info-card', ['quiz' => $quiz, 'user' => $user])
+        ->assertOk()
+        ->assertSet('user', $user);
+
+});
+
+it('set number of completed attempts property for the user', function () {
+    // Arrange
+    $quiz = Quiz::factory()
+        ->create();
+    $user = User::factory()
+        ->create();
+    $user->quizzes()->attach($quiz, ['completed_at' => now()]);
+    $user->quizzes()->attach($quiz, ['completed_at' => now()]);
+    $user->quizzes()->attach($quiz, ['completed_at' => null]);
+
+    // Act & Assert
+    Livewire::test('quiz-info-card', ['quiz' => $quiz, 'user' => $user])
+        ->assertOk()
+        ->assertSet('totalCompletedAttempts', 2);
+
+});
+
+it('set total score property for completed quiz of the user', function () {
+    // Arrange
+    $quiz = Quiz::factory()
+        ->create();
+    $user = User::factory()
+        ->create();
+
+    $user->quizzes()->attach($quiz, ['completed_at' => now(), 'score' => 5]);
+    $user->quizzes()->attach($quiz, ['completed_at' => now(), 'score' => 8]);
+    $user->quizzes()->attach($quiz, ['completed_at' => null, 'score' => 100]);
+
+    // Act & Assert
+    Livewire::test('quiz-info-card', ['quiz' => $quiz, 'user' => $user])
+        ->assertOk()
+        ->assertSet('totalScore', 13);
+
+});
+
+it('shows number of completed attempts for the user', function() {
+    // Arrange
+    $quiz = Quiz::factory()
+        ->create();
+    $user = User::factory()
+        ->create();
+
+    // Act & Assert
+    Livewire::test('quiz-info-card', ['quiz' => $quiz, 'user' => $user])
+        ->assertOk()
+        ->set('totalCompletedAttempts', 2)
+        ->assertSeeHtmlInOrder([
+            "<span id=\"quiz-{$quiz->id}-attempts\"",
+            '2',
+        ]);
+
+ });
+
+it('shows number of achieved score for the user', function() {
+    // Arrange
+    $quiz = Quiz::factory()
+        ->create();
+    $user = User::factory()
+        ->create();
+
+    // Act & Assert
+    Livewire::test('quiz-info-card', ['quiz' => $quiz, 'user' => $user])
+        ->assertOk()
+        ->set('totalScore', 13)
+        ->assertSeeHtmlInOrder([
+            "<span id=\"quiz-{$quiz->id}-score\"",
+            '13',
+        ]);
+
+ });

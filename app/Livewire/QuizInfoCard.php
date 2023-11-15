cf08f863a6fc37910295592febbd1fb7bcb9a6cf
mirchaemanuel <mirchaemanuel@gmail.com>
2023-11-15 12:45:03 +0100

    WIP: QuizCardInfo

--- app/Livewire/QuizInfoCard.php
+++ app/Livewire/QuizInfoCard.php
@@ -3,11 +3,28 @@
 namespace App\Livewire;
 
 use App\Models\Quiz;
+use App\Models\User;
 use Livewire\Component;
 
 class QuizInfoCard extends Component
 {
     public Quiz $quiz;
+
+    public User $user;
+    public int $totalCompletedAttempts;
+    public int $totalScore;
+
+    public function mount(Quiz $quiz, User $user)
+    {
+        $this->quiz = $quiz;
+        $this->user = $user;
+
+        $completedUserQuizAttempts = $this->user->quizzes->whereNotNull('pivot.completed_at');
+
+        $this->totalCompletedAttempts = $completedUserQuizAttempts->count();
+        $this->totalScore = $completedUserQuizAttempts->sum('pivot.score');
+
+    }
 
     public function render()
     {
